809de15893571e91c49996ea08639059677657cf
Omar Aleiser Alvarez Laverde <omaraleyser@hotmail.com>
2023-10-16 18:55:29 -0500

    deploy

--- src/server.js
+++ src/server.js
@@ -1,24 +1,30 @@
-const mongoose = require('mongoose');
-const app = require('./app');
-const connectDB = require('./utils/connection');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const app = require("./app");
+const connectDB = require("./utils/connection");
+require("dotenv").config();
 connectDB(process.env);
 
 connectDB();
+//
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+  );
 
-mongoose.connection.once('open', () => {
-		console.log("Connected to MongoDB");
-})
-
-mongoose.connection.on('error', err => {
-		console.error(err);
-})
-
-const PORT = process.env.PORT || 8080
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  next();
+});
+//
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
 
+const PORT = process.env.PORT || 8080;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
